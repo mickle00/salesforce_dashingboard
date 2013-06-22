c7b05a8f8d089d4fc3dd1c42bd114e616f36f74f
Michael Stewart <mickle00@gmail.com>
2013-06-21 20:35:10 -0700

    Updating cases class and div ids to better represent data

--- jobs/cases.rb
+++ jobs/cases.rb
@@ -1,9 +1,13 @@
 SCHEDULER.every '2s' do
-  send_event('cases', { items: [
-                                {label: 'HSTS', value: '15'},
-                                {label: 'PAC/SAM', value: '15'},
-                                {label: 'Content', value: '15'},
-                                {label: 'Media', value: '15'}
-                               ] 
-                      })
+  send_event('cases-by-team', 
+                          { items: [
+                                    {label: 'HSTS', value: '15'},
+                                    {label: 'PAC/SAM', value: '15'},
+                                    {label: 'Content', value: '15'},
+                                    {label: 'Media', value: '15'}
+                                   ],
+                            title: 'WHAT UP DOGG'
+                          })
+  send_event('cases-opened-today',   { value: rand(100) })
+  send_event('cases-closed-today',   { value: rand(100) })
 end
