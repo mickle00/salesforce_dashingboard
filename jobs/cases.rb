436f9a9a38116ead4dca24e2dfce81d3fbd50983
Michael Stewart <mickle00@gmail.com>
2013-06-22 16:15:46 -0700

    Reducing frequency to stay within governor limits

--- jobs/cases.rb
+++ jobs/cases.rb
@@ -9,7 +9,7 @@ end
 last_x = totalCases.last[:x]
 totalCases << { x: last_x, y: allOpenCases['rowCount']}
 
-SCHEDULER.every '10s' do
+SCHEDULER.every '60s', :first_in => 0 do
   allOpenCases = get_report_summary(execute_report('All_Open_Cases'))
   openCases = get_report_summary(execute_report('Support_Req_Open'))
   closedCases = get_report_summary(execute_report('Support_Req_Closed_Today'))
