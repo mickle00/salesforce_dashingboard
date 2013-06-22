d4f9fec16d48a65c2388e2bd951b98eebbfb31e7
Michael Stewart <mickle00@gmail.com>
2013-06-22 14:30:42 -0700

    updating jobs to use salesforce data

--- jobs/cases.rb
+++ jobs/cases.rb
@@ -1,4 +1,18 @@
-SCHEDULER.every '2s' do
+require "#{File.dirname(__FILE__)}/../lib/salesforce_core"
+
+totalCases = []
+allOpenCases = get_report_summary(execute_report('All_Open_Cases'))
+
+(1..9).each do |i|
+  totalCases << { x: i * 10, y: 0}
+end
+last_x = totalCases.last[:x]
+totalCases << { x: last_x, y: allOpenCases['rowCount']}
+
+SCHEDULER.every '10s' do
+  allOpenCases = get_report_summary(execute_report('All_Open_Cases'))
+  openCases = get_report_summary(execute_report('Support_Req_Open'))
+  closedCases = get_report_summary(execute_report('Support_Req_Closed_Today'))
   send_event('cases-by-team', 
                           { items: [
                                     {label: 'HSTS', value: '15'},
@@ -8,6 +22,12 @@ SCHEDULER.every '2s' do
                                    ],
                             title: 'WHAT UP DOGG'
                           })
-  send_event('cases-opened-today',   { value: rand(100) })
-  send_event('cases-closed-today',   { value: rand(100) })
+  send_event('cases-opened-today',   { value: openCases['rowCount'], title: openCases['title'] })
+  send_event('cases-closed-today',   { value: closedCases['rowCount'], title: closedCases['title'] })
+
+  totalCases.shift
+  last_x += 10
+  totalCases << { x: last_x, y: allOpenCases['rowCount']}
+
+  send_event('allOpenCases', points: totalCases)
 end
