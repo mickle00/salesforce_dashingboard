1c42adbc3b06af235dcf15fb1a0c0ff36160654b
Michael Stewart <mickle00@gmail.com>
2013-06-23 13:30:54 -0700

    adding different chatter placeholder text

--- jobs/chatter.rb
+++ jobs/chatter.rb
@@ -1,7 +1,7 @@
 SCHEDULER.every '10m', :first_in => 0 do |job|
-    tweets = [{ name: 'First', body: '1111', avatar: '' },
-            { name: 'Second', body: '2', avatar: ''},
-            { name: 'Third', body: '2', avatar: ''},
-            { name: 'Fourth', body: '2', avatar: ''}]
+    tweets = [{ name: 'Marc Benioff', body: 'Imagine if this was a Chatter feed!', avatar: 'https://si0.twimg.com/profile_images/3127862205/7a97787536e5b859a5a737db7bef20f4_bigger.jpeg' },
+            { name: 'The Developer', body: 'Ugh. More work, I\'ll just hardcode this for now.', avatar: 'https://si0.twimg.com/profile_images/395743755/LazyDeveloper_73x73_TwitterAvatar_bigger.jpg'},
+            { name: 'Abe Lincoln', body: 'This is going to be huge.', avatar: 'https://si0.twimg.com/profile_images/679430814/alincoln_bigger.jpg'},
+            { name: 'Michael Scott', body: 'That\'s what she said', avatar: 'https://si0.twimg.com/profile_images/105247240/michael_scott_bigger.png'}]
     send_event('chatter', comments: tweets)
 end
