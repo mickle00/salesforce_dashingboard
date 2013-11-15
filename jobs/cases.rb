e33f6e116e7c1ede333bc318d54bfa217ce77dcc
Michael Stewart <mickle00@gmail.com>
2013-11-15 09:44:38 -0800

    Updating to GA Analaytics API Spec

--- jobs/cases.rb
+++ jobs/cases.rb
@@ -9,18 +9,18 @@ end
 last_x = totalCases.last[:x]
 totalCases << { x: last_x, y: allOpenCases['rowCount']}
 
-SCHEDULER.every '60s', :first_in => 0 do
+SCHEDULER.every '20s', :first_in => 0 do
   allOpenCases = get_report_summary(execute_report('All_Open_Cases'))
   openCases = get_report_summary(execute_report('Support_Req_Open'))
   closedCases = get_report_summary(execute_report('Support_Req_Closed_Today'))
   casesByTeam = get_top_level_summary('Support_Req')
   send_event('cases-by-team',{ items: casesByTeam['values'], title: casesByTeam['summary']['title']})
-  send_event('cases-opened-today',   { value: openCases['rowCount'], title: openCases['title'] })
-  send_event('cases-closed-today',   { value: closedCases['rowCount'], title: closedCases['title'] })
+  send_event('cases-opened-today',   { value: openCases['rowCount'][0]['value'], title: openCases['title'] })
+  send_event('cases-closed-today',   { value: closedCases['rowCount'][0]['value'], title: closedCases['title'] })
 
   totalCases.shift
   last_x += 10
-  totalCases << { x: last_x, y: allOpenCases['rowCount']}
+  totalCases << { x: last_x, y: allOpenCases['rowCount'][0]['value']}
 
   send_event('allOpenCases', {title: allOpenCases['title'], points: totalCases})
 end
